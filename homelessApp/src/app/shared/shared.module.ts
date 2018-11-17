9c7b216da160694700e2b54496f9ca0b5532e32f
lchoo <lchoo1996@gmail.com>
2018-11-17 18:00:14 -0500

    refactor: added package, map component

---
+++ homelessApp/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapComponent } from './map/map.component';
+
+const COMPONENTS: any[] =  [
+  MapComponent
+  ]
+];
+
+@NgModule({
+  declarations: [COMPONENTS],
+  exports: [...COMPONENTS],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
