7b2386bfadf6a8236a942a755ffcad4d4cf8b68c
lchoo <lchoo1996@gmail.com>
2018-11-17 16:18:35 -0500

    refactor: change jobs

--- homelessApp/src/app/jobs/jobs.page.ts
+++ homelessApp/src/app/jobs/jobs.page.ts
@@ -1,32 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 
 @Component({
     selector: "app-jobs",
     templateUrl: "jobs.page.html",
     styleUrls: ["jobs.page.scss"],
 })
-export class JobsPage {
-    items = [
-        "Pokémon Yellow",
-        "Super Metroid",
-        "Mega Man X",
-        "The Legend of Zelda",
-        "Pac-Man",
-        "Super Mario World",
-        "Street Fighter II",
-        "Half Life",
-        "Final Fantasy VII",
-        "Star Fox",
-        "Tetris",
-        "Donkey Kong III",
-        "GoldenEye 007",
-        "Doom",
-        "Fallout",
-        "GTA",
-        "Halo",
-    ];
-
-    itemSelected(item: string) {
-        console.log("Selected Item", item);
-    }
-}
+export class JobsPage {}
