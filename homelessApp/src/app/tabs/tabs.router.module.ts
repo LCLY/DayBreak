94898668ae15e84615f799385301823bfecb0cc7
lchoo <lchoo1996@gmail.com>
2018-11-17 12:13:24 -0500

    refactor: change about to jobs

--- homelessApp/src/app/tabs/tabs.router.module.ts
+++ homelessApp/src/app/tabs/tabs.router.module.ts
@@ -1,47 +1,47 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { TabsPage } from './tabs.page';
-import { HomePage } from '../home/home.page';
-import { AboutPage } from '../about/about.page';
-import { ContactPage } from '../contact/contact.page';
+import { TabsPage } from "./tabs.page";
+import { HomePage } from "../home/home.page";
+import { JobsPage } from "../jobs/jobs.page";
+import { ContactPage } from "../contact/contact.page";
 
 const routes: Routes = [
-  {
-    path: 'tabs',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirectTo: '/tabs/(home:home)',
-        pathMatch: 'full',
-      },
-      {
-        path: 'home',
-        outlet: 'home',
-        component: HomePage
-      },
-      {
-        path: 'about',
-        outlet: 'about',
-        component: AboutPage
-      },
-      {
-        path: 'contact',
-        outlet: 'contact',
-        component: ContactPage
-      }
-    ]
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/(home:home)',
-    pathMatch: 'full'
-  }
+    {
+        path: "tabs",
+        component: TabsPage,
+        children: [
+            {
+                path: "",
+                redirectTo: "/tabs/(home:home)",
+                pathMatch: "full",
+            },
+            {
+                path: "home",
+                outlet: "home",
+                component: HomePage,
+            },
+            {
+                path: "jobs",
+                outlet: "jobs",
+                component: JobsPage,
+            },
+            {
+                path: "contact",
+                outlet: "contact",
+                component: ContactPage,
+            },
+        ],
+    },
+    {
+        path: "",
+        redirectTo: "/tabs/(home:home)",
+        pathMatch: "full",
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class TabsPageRoutingModule {}
