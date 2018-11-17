7ace754f4d9849aeb70bb93198daf9074ecfee0a
lchoo <lchoo1996@gmail.com>
2018-11-17 15:50:27 -0500

    refactor: new update

--- homelessApp/src/app/app-routing.module.ts
+++ homelessApp/src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
     { path: "", loadChildren: "./tabs/tabs.module#TabsPageModule" },
-  { path: 'jobDetails', loadChildren: './job-details/job-details.module#JobDetailsPageModule' },
+    {
+        path: "jobDetails",
+        loadChildren: "./job-details/job-details.module#JobDetailsPageModule",
+    },
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- homelessApp/src/app/jobs/jobs.page.ts
+++ homelessApp/src/app/jobs/jobs.page.ts
@@ -1,7 +1,5 @@
 import { Component } from "@angular/core";
 
-import { NavController, NavParams } from "ionic-angular";
-
 @Component({
     selector: "app-jobs",
     templateUrl: "jobs.page.html",
