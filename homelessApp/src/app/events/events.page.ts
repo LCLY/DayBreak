2c4edc8222c7acac221357db77e054434bf5ee7a
ericchan <31045561+ericflyfly@users.noreply.github.com>
2018-11-17 21:31:17 -0500

    added event page

--- homelessApp/src/app/events/events.page.ts
+++ homelessApp/src/app/events/events.page.ts
@@ -1,8 +1,12 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: "app-contact",
-    templateUrl: "events.page.html",
-    styleUrls: ["events.page.scss"],
+	selector: "app-contact",
+	templateUrl: "events.page.html",
+	styleUrls: ["events.page.scss"],
 })
-export class EventsPage {}
+export class EventsPage {
+	constructor() {}
+
+	ngOnInit() {}
+}
--- homelessApp/src/app/upcoming-events/upcoming-events.page.ts
+++ homelessApp/src/app/upcoming-events/upcoming-events.page.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-upcoming-events',
-  templateUrl: './upcoming-events.page.html',
-  styleUrls: ['./upcoming-events.page.scss'],
+	selector: "app-upcoming-events",
+	templateUrl: "./upcoming-events.page.html",
+	styleUrls: ["./upcoming-events.page.scss"],
 })
 export class UpcomingEventsPage implements OnInit {
+	items = ["Star Fox", "Tetris", , "Tetris"];
+	items1 = ["Star Fox", "Tetris", "Tetris"];
+	items2 = ["Star Fox", "Tetris", "Tetris"];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	itemSelected(item: string) {
+		console.log("Selected Item", item);
+	}
 }
